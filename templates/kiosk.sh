04ff3b6a8fe8f4dc01ba08e710f43a42d29498dd
jan wiebe klijnsma <janwiebe@janwiebe.eu>
2022-12-13 20:20:08 +0100

    add .

--- templates/kiosk.sh
+++ templates/kiosk.sh
@@ -6,6 +6,16 @@ export DISPLAY=:0
 # Hide the mouse from the display
 unclutter &
 
+# Disable screensaver
+gsettings set org.gnome.desktop.screensaver logout-enabled false
+gsettings set org.gnome.desktop.screensaver lock-enabled false
+gsettings set org.gnome.desktop.session idle-delay 0
+gsettings set org.gnome.desktop.screensaver idle-activation-enabled false
+gsettings set org.gnome.desktop.screensaver show-notifications false
+gsettings set org.gnome.desktop.screensaver status-message-enabled false
+gsettings set org.gnome.desktop.screensaver ubuntu-lock-on-suspend false
+gsettings set org.gnome.settings-daemon.plugins.screensaver-proxy active false
+
 # Disable general blanking
 xset -dpms
 xset s noblank
