af870813539641aa01f1b0a57ece3ea52d0d2966
jan wiebe klijnsma <janwiebe@janwiebe.eu>
2022-12-13 19:28:17 +0100

    add .

--- templates/kiosk.sh
+++ templates/kiosk.sh
@@ -6,7 +6,10 @@ export DISPLAY=:0
 # Hide the mouse from the display
 unclutter &
 
+# Disable general blanking
+xset -dpms
+xset s noblank
+xset s off
+
 #!/bin/bash
-X_RES=\`xrandr | grep \"*\" | awk -Fx '{ print \$1 }' | sed 's/[^0-9]*//g'\`\n\
-Y_RES=\`xrandr | grep \"*\" | awk -Fx '{ print \$2 }' | awk '{ print \$1 }'\`\n\n\
-/usr/bin/google-chrome --kiosk --start-fullscreen --window-position=0,0 --window-size=\$X_RES,\$Y_RES --no-first-run --incognito --no-default-browser-check --disable-translate https://google.com
+/usr/bin/google-chrome --kiosk --start-fullscreen --window-position=0,0 --window-size=1920,1080 --no-first-run --incognito --no-default-browser-check --disable-translate https://google.com
