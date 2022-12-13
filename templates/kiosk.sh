1ec1053d701283b42c285a8704e0471b3296504c
jan wiebe klijnsma <janwiebe@janwiebe.eu>
2022-12-13 19:18:37 +0100

    add .

--- templates/kiosk.sh
+++ templates/kiosk.sh
@@ -6,7 +6,7 @@ export DISPLAY=:0
 # Hide the mouse from the display
 unclutter &
 
-#!/bin/bash\n\n\
+#!/bin/bash
 X_RES=\`xrandr | grep \"*\" | awk -Fx '{ print \$1 }' | sed 's/[^0-9]*//g'\`\n\
 Y_RES=\`xrandr | grep \"*\" | awk -Fx '{ print \$2 }' | awk '{ print \$1 }'\`\n\n\
 /usr/bin/google-chrome --kiosk --start-fullscreen --window-position=0,0 --window-size=\$X_RES,\$Y_RES --no-first-run --incognito --no-default-browser-check --disable-translate https://google.com
